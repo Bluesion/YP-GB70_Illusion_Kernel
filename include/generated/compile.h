4711b730db997559d0bc8fa8ceeba9e36f3cb2ae
gpillusion <dnjscjf098@nate.com>
2013-12-07 19:04:54 +0900

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 15 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#15 PREEMPT Sat Dec 7 14:25:07 KST 2013"
+#define LINUX_COMPILE_TIME "14:25:07"
+#define LINUX_COMPILE_BY "illusion"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
